a046192a26666d58bff6b51b19e02cebb6a5647e
Evasionally <91702636+Evasionally@users.noreply.github.com>
2024-01-17 19:52:38 -0500

    Start Screen
    Added basic UI for start screen. Does not include load or button press animation.

--- lib/screens/startup_screen.dart
+++ lib/screens/startup_screen.dart
@@ -7,21 +7,57 @@ class StartupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Startup screen with animation"),
-            ElevatedButton(
-              onPressed: () => Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AvatarHomeScreen(),
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/StartupBackground.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.185,
+                  bottom: 10,
+                ),
+                child: Image.asset(
+                  "assets/images/AppIcon.png",
+                  height: 180,
                 ),
               ),
-              child: const Text("Start"),
-            ),
-          ],
+              Container(
+                margin: EdgeInsets.only(bottom: 225),
+                child: const Text(
+                  "Explore",
+                  style: TextStyle(
+                    color: Color(0xfff6f6f6),
+                    fontSize: 70,
+                    fontFamily: 'Fredoka',
+                  ),
+                ),
+              ),
+              IconButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff9173f4),
+                  minimumSize: Size(140, 40),
+                ),
+                onPressed: () => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AvatarHomeScreen(),
+                  ),
+                ),
+                icon: const Icon(
+                  Icons.play_arrow,
+                  color: Color(0xfff6f6f6),
+                  size: 45,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
