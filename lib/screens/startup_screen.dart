bf70999e200e9aea7f5ac78066db53c64e524ca0
Evasionally <91702636+Evasionally@users.noreply.github.com>
2024-01-20 17:29:07 -0500

    Added Leaderboard
    Created score-bar and top-player widgets. Added them to the leaderboard screen

--- lib/screens/startup_screen.dart
+++ lib/screens/startup_screen.dart
@@ -7,58 +7,62 @@ class StartupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DecoratedBox(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/StartupBackground.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.185,
-                  bottom: 10,
-                ),
-                child: Image.asset(
-                  "assets/images/AppIcon.png",
-                  height: 180,
-                ),
+      body: Stack(
+        children: [
+          DecoratedBox(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/StartupScreen.png"),
+                fit: BoxFit.cover,
               ),
-              Container(
-                margin: EdgeInsets.only(bottom: 225),
-                child: const Text(
-                  "Explore",
-                  style: TextStyle(
-                    color: Color(0xfff6f6f6),
-                    fontSize: 70,
-                    fontFamily: 'Fredoka',
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.185,
+                      bottom: 10,
+                    ),
+                    child: Image.asset(
+                      "assets/images/AppIcon.png",
+                      height: 180,
+                    ),
                   ),
-                ),
-              ),
-              IconButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff9173f4),
-                  minimumSize: Size(140, 40),
-                ),
-                onPressed: () => Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const AvatarHomeScreen(),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 225),
+                    child: const Text(
+                      "Explore",
+                      style: TextStyle(
+                        color: Color(0xfff6f6f6),
+                        fontSize: 70,
+                        fontFamily: 'Fredoka',
+                      ),
+                    ),
                   ),
-                ),
-                icon: const Icon(
-                  Icons.play_arrow,
-                  color: Color(0xfff6f6f6),
-                  size: 45,
-                ),
+                  IconButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff9173f4),
+                      minimumSize: Size(140, 40),
+                    ),
+                    onPressed: () => Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AvatarHomeScreen(),
+                      ),
+                    ),
+                    icon: const Icon(
+                      Icons.play_arrow,
+                      color: Color(0xfff6f6f6),
+                      size: 45,
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
