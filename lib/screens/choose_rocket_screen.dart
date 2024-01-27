f8f267eec5a8975f8a80753f419fdb38f358e8f0
rachel <m3m3ntomori2@gmail.com>
2024-01-27 15:37:45 -0500

    screen layout other than staggaring

--- lib/screens/choose_rocket_screen.dart
+++ lib/screens/choose_rocket_screen.dart
@@ -1,87 +1,108 @@
-import 'package:explore/screens/choose_avatar_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:explore/app_colors.dart';
+import 'package:explore/screens/choose_avatar_screen.dart';
 
-class ChooseRocketScreen extends StatelessWidget {
-  const ChooseRocketScreen({super.key});
+class ChooseRocketScreen extends StatefulWidget {
+  const ChooseRocketScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChooseRocketScreenState createState() => _ChooseRocketScreenState();
+}
+
+class _ChooseRocketScreenState extends State<ChooseRocketScreen> {
+  String selectedRocket = "";
 
   @override
   Widget build(BuildContext context) {
-    bool rocketSelected = false;
+    double screenWidth = MediaQuery.of(context).size.width;
+    double rocketSize = screenWidth * 0.3; // 30% of the screen width
+    double spacing = screenWidth * 0.03; // 3% of the screen width
+
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          // Navigate back when the back button is pressed
-          onPressed: () => Navigator.pop(context),
+        leading: Container(
+          decoration: BoxDecoration(
+            color: AppColors.darkPurple,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: AppColors.white,
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/StarsBackground.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildRocketAvatar(Icons.rocket_launch_outlined, rocketSize),
+                  SizedBox(width: spacing),
+
+                  buildRocketAvatar(Icons.rocket_outlined, rocketSize),
+                ],
+              ),
+              SizedBox(height: spacing),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildRocketAvatar(Icons.rocket_launch, rocketSize),
+                  SizedBox(width: spacing),
+                  buildRocketAvatar(Icons.rocket, rocketSize),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-            onTap: () {
-              rocketSelected = true;
-      },
-            child: Container(
-              height:100,
-              width: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: rocketSelected ? Colors.grey: Colors.blue,
-              ),
-              margin: const EdgeInsets.all(10.0),
-              child: Image.asset('assets/rocketImage1.jpg'
-              ),
-            ),
-            ),
-            Container(
-              height:100,
-              width: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue,
-              ),
-              margin: const EdgeInsets.all(10.0),
-              child: Image.asset('assets/rocketImage2.jpg'
-              ), // Image.asset
-            ),
-            Container(
-              height:100,
-              width: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue,
-              ),
-              margin: const EdgeInsets.all(10.0),
-              child: Image.asset('assets/rocketImage3.jpg'
-              ), // Image.asset
-            ),
-            Container(
-              height:100,
-              width: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue,
-              ),
-              margin: const EdgeInsets.all(10.0),
-              child: Image.asset('assets/rocketImage4.jpg'
-              ), // Image.asset
-            ),
+    );
+  }
 
-            IconButton(
-              icon: const Icon(Icons.check),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ChooseAvatarScreen(),
-                ),
-              ),
+  Widget buildRocketAvatar(IconData icon, double size) {
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: selectedRocket == icon.toString() ? AppColors.lightGrey : AppColors.darkBlue,
+      ),
+      margin: const EdgeInsets.all(10.0),
+      child: IconButton(
+        icon: Icon(icon),
+        onPressed: () {
+          selectRocket(icon.toString());
+          navigateToChooseAvatarScreen();
+        },
+      ),
+    );
+  }
 
-            ),
-      ],
+  void selectRocket(String rocket) {
+    setState(() {
+      selectedRocket = rocket;
+    });
+  }
 
-        ),
+  void navigateToChooseAvatarScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const ChooseAvatarScreen(),
       ),
     );
   }
