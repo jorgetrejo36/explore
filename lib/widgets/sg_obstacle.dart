7f6efe89d5f05e99fd99c7de863a3f7c7bc4ed5f
Kevin <55450760+kevinbopp@users.noreply.github.com>
2024-02-19 11:38:24 -0500

    Finished random animated dropping obstacles, added retry window

--- lib/widgets/sg_obstacle.dart
+++ lib/widgets/sg_obstacle.dart
@@ -1,73 +1,211 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_svg/svg.dart';
+import 'shooting_game.dart';
 
-class SGObstacle extends StatelessWidget {
+// Assigned to all obstacles that do not yet have an answer option.
+const int emptyObstacle = 9999;
+// Assigned to obstacles if they are used on the 1st try to correctly
+// solve a problem.
+const int correctObstacle = -9999;
+// Assigned to obstacles if they have been filled before.
+const int filledObstacle = 99999;
+
+class SGObstacle extends StatefulWidget {
   // Shooting Game obstacle properties.
 
-  // Stores the image path to the obstacle and the reward once it's broken.
+  // Stores the image paths to the obstacle, its reward, and destroyed variant.
   final String obstacleImg;
   final String obstacleRewardImg;
+  final String obstacleDestroyedImg;
 
   // Is this obstacle destroyed?
-  final bool isDestroyed;
+  late bool isDestroyed;
+  // Is this obstacle dropping?
+  late bool isDropping;
+
+  // Is this entire obstacle and everything else visible?
+  late bool alive;
 
   // What is the answer displayed on this obstacle?
-  final int answerValue;
+  late int answerValue;
 
   // What is the index of this obstacle in the list of all obstacles?
+  // There are 3 obstacles per question, so this is [0-(3*numProblems - 1)].
   final int obstacleIndex;
 
-  // What problem number is this obstacle associated with?
-  final int problemNum;
-
+  // At what X value is this obstacle currently at?
+  final double obstacleX;
   // At what Y value is this obstacle currently at?
-  final double obstacleHeight;
+  final double obstacleY;
+  // What column is this obstacle in?
+  final String obstacleCol;
 
-  // Constructor
-  const SGObstacle({
+  // Constructor; all obstacles begin with no (empty) values.
+  SGObstacle({
     Key? key,
     required this.obstacleImg,
     required this.obstacleRewardImg,
+    required this.obstacleDestroyedImg,
     this.isDestroyed = false,
-    required this.answerValue,
+    this.answerValue = emptyObstacle,
+    this.isDropping = false,
+    this.alive = true,
     required this.obstacleIndex,
-    required this.problemNum,
-    required this.obstacleHeight,
+    required this.obstacleX,
+    required this.obstacleY,
+    required this.obstacleCol,
   }) : super(key: key);
 
   @override
+  _SGObstacleState createState() => _SGObstacleState();
+
+}
+
+class _SGObstacleState extends State<SGObstacle> with TickerProviderStateMixin {
+  // Stores the Y position of this obstacle.
+  late double obstacleY;
+  // The ticker used to repeatedly drop this obstacle.
+  late Ticker dropTicker;
+  // Gets assigned in build(); same as rocketYPos in shooting_game.dart.
+  late double rocketYTop;
+
+  // How fast should this obstacle drop?
+  double dropRate = 0.3;
+
+  @override
+  void initState() {
+    super.initState();
+    obstacleY = widget.obstacleY;
+    widget.alive = true;
+
+    // Create a ticker and instruct it to move the obstacle on every tick.
+    dropTicker = createTicker((_) {
+      // Every tick, move the obstacle down
+      if (widget.isDropping)
+        {
+          setState(() {
+            obstacleY += dropRate; // Adjust the rate of falling as needed
+          });
+
+          // Check if it has passed the yValue of the rocket.
+          if (obstacleY >= rocketYTop)
+            {
+              // This obstacle has collided with the rocket.
+              shootingGameStateInstance?.collideRocket(widget.obstacleIndex);
+            }
+        }
+    });
+
+    dropTicker.start();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: obstacleHeight,
-      child: GestureDetector(
-        onTap: () {
-          // Handle obstacle click
-          print("Obstacle clicked: Index $obstacleIndex");
-        },
-        child: Column(
-          children: [
+    rocketYTop = MediaQuery.of(context).size.height * 0.48;
 
-            // SVG image for obstacle
-            SvgPicture.asset(
-              obstacleImg,
-              width: 50,
-              height: 50,
-            ),
-
-            // Text overlay displaying value
-            Text(
-              answerValue.toString(),
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                fontFamily: "Fredoka",
+    return Visibility(
+      visible: widget.alive,
+      child: Positioned(
+        left: widget.obstacleX,
+        top: obstacleY,
+        child: GestureDetector(
+          onTap: () {
+            // Attempt to shoot the obstacle.
+            shootingGameStateInstance?.shootObstacle(widget.obstacleIndex);
+          },
+          // The SMASHED image should also stop moving, so it should be a diff
+          // object and, when created, appear at the exact position of the origin
+          // obstacle.
+          child: Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.center,
+            children: [
+              // SVG image for reward (if answered correctly)
+              Visibility(
+                visible: widget.isDestroyed && widget.answerValue == correctObstacle,
+                child: Positioned(
+                  top: -54,
+                  child: SvgPicture.asset(
+                    widget.obstacleRewardImg,
+                    width: 56,
+                    height: 56,
+                  ),
+                ),
               ),
-            ),
 
-          ],
+              // SVG image for obstacle (when not destroyed)
+              Visibility(
+                visible: !widget.isDestroyed,
+                child: SvgPicture.asset(
+                  widget.obstacleImg,
+                  width: 92,
+                  height: 92,
+                ),
+              ),
+
+              // SVG image for obstacle (when destroyed)
+              Visibility(
+                visible: widget.isDestroyed,
+                child: SvgPicture.asset(
+                  widget.obstacleDestroyedImg,
+                  width: 92,
+                  height: 92,
+                ),
+              ),
+
+              // Text overlay displaying value
+              Visibility(
+                visible: (widget.answerValue != emptyObstacle &&
+                          widget.answerValue != correctObstacle),
+                child: Stack(
+                  children: [
+                    // White text for answer value.
+                    Visibility(
+                      visible: !widget.isDestroyed,
+                      child: Text(
+                        widget.answerValue.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Fredoka",
+                            shadows: [
+                              Shadow( // bottomLeft
+                                  offset: Offset(-1.5, -1.5),
+                                  color: Colors.black
+                              ),
+                              Shadow( // bottomRight
+                                  offset: Offset(1.5, -1.5),
+                                  color: Colors.black
+                              ),
+                              Shadow( // topRight
+                                  offset: Offset(1.5, 1.5),
+                                  color: Colors.black
+                              ),
+                              Shadow( // topLeft
+                                  offset: Offset(-1.5, 1.5),
+                                  color: Colors.black
+                              ),
+                            ]
+                        ),
+                      ),
+                    ),
+
+
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    dropTicker.dispose();
+    super.dispose();
   }
 }
--- lib/widgets/shooting_game.dart
+++ lib/widgets/shooting_game.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+import 'package:explore/widgets/sg_obstacle.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 // ignore: unused_import
 import '../screens/game_result_screen.dart';
 import '../screens/planet_map_screen.dart';
@@ -9,8 +12,6 @@ import 'package:explore/widgets/retry_window.dart';
 import 'package:explore/utils/problem_generator.dart';
 import 'package:explore/widgets/sg_info_bar.dart';
 import 'package:explore/widgets/sg_player_rocket.dart';
-// ignore: unused_import
-import 'package:explore/widgets/sg_obstacle_container.dart';
 
 class ShootingGameStateful extends StatefulWidget {
   const ShootingGameStateful(
@@ -28,6 +29,12 @@ class ShootingGameStateful extends StatefulWidget {
 class _ShootingGameState extends State<ShootingGameStateful> {
   // How many problems does the player have to answer for this game?
   int numProblems = 5;
+  // How many obstacles (i.e. asteroids) should spawn per problem?
+  int numObstaclesPerProblem = 3;
+  // Stores how many obstacles will be generated in total.
+  late int numObstacles;
+  // Define a list to store generated obstacles.
+  late List<SGObstacle> obstacles;
   // How many lives does the player have left? Starts with 3.
   int livesLeft = 3;
   // How many problems in total has the player answered (right or wrong)?
@@ -37,11 +44,24 @@ class _ShootingGameState extends State<ShootingGameStateful> {
   int problemsCorrect = 0;
   int problemsWrong = 0;
   // Stores the answer choice the player selects.
-  int answerChoice = 0;
-  // Has the player answered the current problem yet?
-  bool hasAnswered = false;
+  late int answerChoice;
+  // Is the player allowed to click an answer?
+  bool canSolve = false;
   // Has the game started?
   bool gameStarted = false;
+  // Are the obstacles dropping?
+  bool isDropping = false;
+
+  // What should the range in pixels be that obstacles can randomly spawn in?
+  double obstacleRangeY = 150;
+
+  // How high should the obstacles begin to be generated?
+  // Currently, they begin off-screen. Leave as such.
+  double initialHeight = 100;
+
+  // How much space should there be between
+  // every set of obstacles per question?
+  double obstaclePadding = 200;
 
   // Stores the problem the player has to answer.
   late GeneratedProblem problem = widget.shootingProblem.generateProblem();
@@ -63,18 +83,244 @@ class _ShootingGameState extends State<ShootingGameStateful> {
   // What column is the player in?
   late String rocketColumn = "center";
 
+  // Called shortly after the state loads with its dependencies.
+  // Used in place of initState() for initial game loading/setup.
+  // Note: This is still called before build().
   @override
   void didChangeDependencies() {
-    // Sets player rocket to center ground on initialization.
     super.didChangeDependencies();
-    rocketXPos = MediaQuery.of(context).size.width / 2 - 35;
-    rocketYPos = MediaQuery.of(context).size.height - 255;
+
+    // Apply the themed image paths to all assets in the game.
+    applyTheme();
+
+    // Set up the game (generate a list of & spawn obstacles, create
+    // falling obstacle ticker to animate them.
+    setUpGame();
+  }
+
+  void applyTheme()
+  {
+    // Switch planet, use based on shooting_themes.dart. Remove unused import.
+  }
+
+  // Function to generate random heights within a range
+  double getRandomHeight(double minHeight, double maxHeight) {
+    final random = Random();
+    return minHeight + random.nextDouble() * (maxHeight - minHeight);
+  }
+
+  void setUpGame()
+  {
+    // Assign the instance.
+    shootingGameStateInstance = this;
+
+    // Rocket position setup (center on ground, turned off).
+    rocketXPos = MediaQuery.of(context).size.width * 0.42;
+    rocketYPos = MediaQuery.of(context).size.height * 0.75;
     rocketColumn = "center";
 
     // Set column positions
-    leftX = rocketXPos - 150;
+    leftX = rocketXPos - (MediaQuery.of(context).size.width * 0.3);
     centerX = rocketXPos;
-    rightX = rocketXPos + 150;
+    rightX = rocketXPos + (MediaQuery.of(context).size.width * 0.3);
+
+    // Identify the number of obstacles to create.
+    numObstacles = numProblems * numObstaclesPerProblem;
+
+    // Initialize the current height range
+    double currentMinHeight = initialHeight;
+    double currentMaxHeight = initialHeight + obstacleRangeY;
+
+    obstacles = List.generate(numObstacles, (index) {
+      String col;
+      double xPos;
+
+      switch (index % 3)
+      {
+        case 0:
+          col = "left";
+          xPos = leftX - 4;
+          break;
+        case 1:
+          col = "center";
+          xPos = centerX - 4;
+          break;
+        default:
+          col = "right";
+          xPos = rightX - 4;
+      }
+
+      // Generate obstacleY based on the current height range
+      double obstacleYVal = getRandomHeight(currentMinHeight, currentMaxHeight);
+
+      // Update the current height range after every set of 3 obstacles
+      if ((index + 1) % 3 == 0) {
+        currentMinHeight = currentMaxHeight + obstaclePadding;
+        currentMaxHeight = currentMinHeight + obstacleRangeY;
+      }
+
+      return SGObstacle(
+        obstacleImg: "assets/images/sg_asteroid_earth.svg",
+        obstacleRewardImg: "assets/images/diamond.svg",
+        obstacleDestroyedImg: "assets/images/sg_asteroid_destroyed_earth.svg",
+        obstacleIndex: index,
+        obstacleX: xPos,
+        obstacleY: -1 * obstacleYVal,
+        obstacleCol: col,
+      );
+    });
+
+  }
+
+  void shootObstacle(int obstacleID)
+  {
+    // Get a reference to this obstacle we tapped.
+    SGObstacle thisObs = obstacles[obstacleID];
+
+    if (!canSolve || !thisObs.isDropping)
+      {
+        // Don't shoot an asteroid that has already stopped moving or
+        // if we aren't allowed to solve things yet.
+        return;
+      }
+
+    setState(() {
+      if (thisObs.obstacleCol == "left")
+      {
+        // Move the rocket left.
+        moveRocket("left");
+        thisObs.isDestroyed = true;
+        thisObs.isDropping = false;
+      }
+      else if (thisObs.obstacleCol == "center")
+      {
+        // Move the rocket center.
+        moveRocket("center");
+        thisObs.isDestroyed = true;
+        thisObs.isDropping = false;
+      }
+      else
+      {
+        // Move the rocket right.
+        moveRocket("right");
+        thisObs.isDestroyed = true;
+        thisObs.isDropping = false;
+      }
+    });
+
+
+
+    int answerPicked = thisObs.answerValue;
+    print("Clicked answer choice $answerPicked.");
+  }
+
+  void loseGame()
+  {
+    // Stop the game and obstacles from continuing.
+    canSolve = false;
+    for (int i = 0; i < numObstacles; i++)
+    {
+      obstacles[i].isDropping = false;
+    }
+
+    // Turn the rocket off.
+    rocketOn = false;
+
+    // Show the retry window.
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Center(
+          child: Text(
+            '$problemsCorrect / $numProblems',
+            style: const TextStyle(
+              fontFamily: 'Fredoka',
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          Center(
+            child: problemsCorrect / numProblems < 0.8
+            // Retry game
+                ? IconButton(
+              icon: SvgPicture.asset(
+                'assets/images/reload.svg',
+                colorFilter:
+                ColorFilter.mode(Colors.black, BlendMode.srcIn),
+                semanticsLabel: "arrow pointing in circle",
+                height: 50,
+                width: 50,
+              ),
+              onPressed: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ShootingGameStateful(
+                      planet: widget.planet,
+                      shootingProblem: widget.shootingProblem),
+                ),
+              ),
+            )
+                : ElevatedButton(
+              // Game result screen
+              onPressed: () => {
+                Navigator.pop(context),
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const GameResultScreen(),
+                  ),
+                ),
+              },
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+              ),
+              child: const Icon(
+                Icons.arrow_right_rounded,
+                color: Colors.black,
+                size: 60,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void loseLife()
+  {
+    // Lose a life.
+    livesLeft -= 1;
+
+    // Update the lives indicator. Needs state conversion first.
+
+    print("Lives left: $livesLeft");
+
+    // Check; if we have no lives left, we have lost.
+    if (livesLeft == 0)
+      {
+        loseGame();
+      }
+  }
+
+  void collideRocket(int obstacleID)
+  {
+    // If this is called, an obstacle has collided with our rocket.
+
+    // First, grab any important info from the obstacle.
+    // int obstacleValue = obstacles[obstacleID].answerValue;
+
+    // Hide the obstacle.
+    obstacles[obstacleID].isDropping = false;
+    obstacles[obstacleID].isDestroyed = true;
+    obstacles[obstacleID].alive = false;
+    obstacles[obstacleID].answerValue = filledObstacle;
+
+    // Update lives.
+    loseLife();
+
+    // Finally, check. If that obstacle contained the CORRECT answer to the
+    // current problem, we need to get rid of the other obstacles and
+    // skip the problem; it's a loss.
   }
 
   // Function to move the rocket
@@ -101,11 +347,9 @@ class _ShootingGameState extends State<ShootingGameStateful> {
       else if (column == "up1")
         {
           // Game starting, move rocket UP.
-          rocketYPos -= 175;
+          rocketYPos -= (MediaQuery.of(context).size.height * 0.185);
         }
     });
-
-    print("Player rocket moved to $rocketColumn column.");
   }
 
   // Generate a new problem.
@@ -115,12 +359,8 @@ class _ShootingGameState extends State<ShootingGameStateful> {
     });
   }
 
-
-  void dropObstacles()
-  {
-    print("Dropping obstacles.");
-  }
-
+  // Start the game (turn rocket on and move it up, create first
+  // question and answers, drop obstacles and show first problem.
   void startGame()
   {
     // Start the game, move the rocket up, and load questions!
@@ -132,8 +372,21 @@ class _ShootingGameState extends State<ShootingGameStateful> {
     moveRocket("up1");
     rocketOn = true;
 
+    for (int i = 0; i < numObstacles; i++)
+    {
+      obstacles[i].isDropping = true;
+    }
+
     // Load the first problem.
     // nextProblem() here.
+
+    // Temporary; fixing state in SGInfoBar first.
+    obstacles[0].answerValue = 1;
+    obstacles[1].answerValue = 0;
+    obstacles[2].answerValue = 2;
+
+    // Allow solving.
+    canSolve = true;
   }
 
   @override
@@ -163,7 +416,14 @@ class _ShootingGameState extends State<ShootingGameStateful> {
 
             // Navigate back when the back button is pressed.
             // Will be an issue on game end screen, update!
-            onPressed: () => Navigator.pop(context),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                // Replace 0 with the int value of the planet attached to this
+                // shooting game instance.
+                builder: (context) => const PlanetMapScreen(selectedPlanet: 0),
+              ),
+            )
           ),
         ),
       ),
@@ -200,7 +460,7 @@ class _ShootingGameState extends State<ShootingGameStateful> {
             ),
           ),
 
-          // Ground (ground_art) for the Earth shooting game.
+          // Ground (ground_art) for the shooting game.
           Visibility(
             visible: !gameStarted,
             child: Positioned(
@@ -214,6 +474,13 @@ class _ShootingGameState extends State<ShootingGameStateful> {
             ),
           ),
 
+          // Player rocket:
+          SGPlayerRocket(
+            isOn: rocketOn,
+            rocketX: rocketXPos,
+            rocketY: rocketYPos,
+          ),
+
           // Game Info bar (displays question, lives, rewards)
           const Positioned(
             bottom: 0,
@@ -222,57 +489,9 @@ class _ShootingGameState extends State<ShootingGameStateful> {
             child: SGInfoBar(),
           ),
 
-          // Player rocket:
-          SGPlayerRocket(
-              isOn: rocketOn,
-              rocketX: rocketXPos,
-              rocketY: rocketYPos,
-          ),
-
-          // Right answer button.
-          Visibility(
-            visible: gameStarted,
-            child: Positioned(
-              bottom: 16,
-              right: 16,
-              child: TextButton(
-                onPressed: () {
-                  moveRocket("right");
-                },
-                child: Text('Right Answer'),
-              ),
-            ),
-          ),
-
-          // Center answer button
-          Visibility(
-            visible: gameStarted,
-            child: Positioned(
-              bottom: 16,
-              left: MediaQuery.of(context).size.width / 3,
-              child: TextButton(
-                onPressed: () {
-                  moveRocket("center");
-                },
-                child: Text('Center Answer'),
-              ),
-            ),
-          ),
-
-          // Left answer button
-          Visibility(
-            visible: gameStarted,
-            child: Positioned(
-              bottom: 16,
-              left: 16,
-              child: TextButton(
-                onPressed: () {
-                  moveRocket("left");
-                },
-                child: Text('Left Answer'),
-              ),
-            ),
-          ),
+          for (int i = 0; i < numObstacles; i++)
+            obstacles[i],
+          // obstacles[0],
 
           Center(
             child: Visibility(
@@ -292,27 +511,13 @@ class _ShootingGameState extends State<ShootingGameStateful> {
             ),
           ),
 
-          // Button to make objects fall
-          Center(
-            child: Visibility(
-              visible: gameStarted,
-              child: ElevatedButton(
-                onPressed: dropObstacles,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white, // Set button color to white
-                ),
-                child: const Text('Drop Obstacles',  style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                )
-                ),
-              ),
-            ),
-          ),
-
         ],
       ),
     );
   }
 }
+
+// Global reference to this specific game just so we can call collideRocket
+// when SGObstacles collide with the rocket. It's not a clean way of doing
+// this but there's no other way due to Flutter.
+_ShootingGameState? shootingGameStateInstance;
